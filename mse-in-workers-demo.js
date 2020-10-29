cea2a1df5b8f4a80945fa79dc7b8c52db966eb16
Matt Wolenetz <wolenetz@chromium.org>
2020-10-28 21:50:30 -0700

    Fix typo and increase work required to buffer (decreased append-size)

--- mse-in-workers-demo.js
+++ mse-in-workers-demo.js
@@ -4,7 +4,7 @@ const MEDIA_URL = 'test-5seconds.webm';
 const MEDIA_TYPE = 'video/webm; codecs="vp9"';
 // Demo is able to show more effectively the main vs worker difference when
 // buffering tiny chunks at a time.
-const APPEND_SIZE = 1024;
+const APPEND_SIZE = 512;
 const BUSYWAIT_DURATION_MILLISECONDS = 800;
 
 // End of demo parameters.
